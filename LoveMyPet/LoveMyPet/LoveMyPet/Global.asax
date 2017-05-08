106d66746875cd0b751198fb3ddfb2c91b4dab9b
FLORENCIA VIDAL <42567367@ort>
2017-05-08 11:45:10 -0300

    Modifique access y agregue c#

---
+++ LoveMyPet/LoveMyPet/LoveMyPet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoveMyPet.Global" Language="C#" %>
---
+++ LoveMyPet/LoveMyPet1/LoveMyPet1/Controllers/RegistrarController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.OleDb;
+using System.Data;
+
+namespace LoveMyPet1.Controllers
+{
+    public class RegistrarController : Controller
+    {
+        
+
+        public ActionResult Index()
+        {
+            string Proveedor = @"Provider=Microsoft.Jet.OLDB.4.0;DataSource=Z:\LoveMyPet\LoveMyPet1\LoveMyPet.mdb";
+        OleDbConnection conn= new OleDbConnection();
+        conn.ConnectionString = Proveedor;
+        conn.Open();
+            return View();
+        }
+
+    }
+}
